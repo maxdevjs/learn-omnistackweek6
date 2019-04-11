9ebb72bdd5b6a9cab1ad328cc1925d853c045489
Massimiliano Giroldi <max.devjs@gmail.com>
2019-04-11 00:06:28 -0300

    Add socket.io and cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,30 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
+const cors = require('cors');
 
 const port = 3333;
 const app = express();
 
+app.use(cors());
+
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+io.on('connection', socket => {
+  socket.on('connectRoom', box => {
+    socket.join(box);
+  })
+});
+
 mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-jxhrd.mongodb.net/omistack?retryWrites=true', {
   useNewUrlParser: true
+});
+
+app.use((req, res, next) => {
+  req.io = io;
+
+  return next();
 });
 
 app.use(express.json());
@@ -15,6 +33,6 @@ app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp')));
 
 app.use(require('./routes'));
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server started on ${port}`);
 });
