521942df4b22ffa116f75d3d9d5ad88e0ce0df48
Massimiliano Giroldi <max.devjs@gmail.com>
2019-04-10 21:03:12 -0300

    Set up moongose

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const BoxController = require('./controllers/BoxController');
 
 const routes = express.Router();
 
 routes.get('/test', (req, res) => {
-  return res.send("value from routes");
+  return res.send("value from routes!!!");
 });
+
+routes.post('/boxes', BoxController.store);
 
 module.exports = routes;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const port = 3333;
 const app = express();
+
+mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-jxhrd.mongodb.net/omistack?retryWrites=true', {
+  useNewUrlParser: true
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
