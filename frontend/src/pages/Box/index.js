a0c57cca2d4adec781b741148072a4f00cf1215a
Massimiliano Giroldi <max.devjs@gmail.com>
2019-04-12 04:29:21 -0300

    Realtime

--- frontend/src/pages/Box/index.js
+++ frontend/src/pages/Box/index.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import { distanceInWords } from 'date-fns';
+import pt from 'date-fns/locale/pt';
+import Dropzone from 'react-dropzone';
+import socket from 'socket.io-client';
 
 import api from '../../services/api';
 import logo from '../../assets/logo.svg';
@@ -9,11 +13,37 @@ import { MdInsertDriveFile } from 'react-icons/md';
 export default class Box extends Component {
   state = { box: {} };
   async componentDidMount() {
+    this.suscribeToNewFiles();
+
     const box = this.props.match.params.id;
     const response = await api.get(`boxes/${box}`);
 
     this.setState({
       box: response.data,
+    });
+  }
+
+  suscribeToNewFiles = () => {
+    const box = this.props.match.params.id;
+    const io = socket('http://localhost:3333');
+
+    io.emit('connectRoom', box);
+
+    io.on('file', data => {
+      this.setState({ 
+        box: {...this.state.box,
+        files: [data, ...this.state.box.files]}
+       });
+    });
+  }
+
+  handleUpload = (files) => {
+    files.forEach(file => {
+      const data = new FormData();
+      const box = this.props.match.params.id;
+
+      data.append('file', file);
+      api.post(`boxes/${box}/files`, data);
     });
   }
 
@@ -25,15 +55,24 @@ export default class Box extends Component {
           <h1>{this.state.box.title}</h1>
         </header>
 
+          <Dropzone onDropAccepted={this.handleUpload}>
+            {({getRootProps, getInputProps}) => (
+              <div className="upload" {...getRootProps()}>
+                <input {...getInputProps()}/>
+                <p>Arraste arquivos ou clique aqui</p>
+              </div>
+            )}
+          </Dropzone>
+
         <ul>
           { this.state.box.files && this.state.box.files.map(file => (
-            <li>
+            <li key={file._id}>
             <a className="fileInfo" href={file.url} target="_blank">
               <MdInsertDriveFile size={24} color="#A5Cfff" />
               <strong>{file.title}</strong>
             </a>
 
-            <span>{file.createdAt}</span>
+            <span>{" "} ha' {distanceInWords(file.createdAt, new Date(), { locale: pt })}</span>
           </li>
           )) }
         </ul>
