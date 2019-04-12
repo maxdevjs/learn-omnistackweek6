28935db89a11190a61a23dda7de0ea4cd68a7261
Massimiliano Giroldi <max.devjs@gmail.com>
2019-04-12 02:42:37 -0300

    Create api

--- frontend/src/pages/Main/index.js
+++ frontend/src/pages/Main/index.js
@@ -1,15 +1,42 @@
 import React, { Component } from 'react';
+import api from '../../services/api';
 
 import logo from '../../assets/logo.svg';
 import './styles.css';
 
 export default class Main extends Component {
+
+  state = {
+    newBox: '',
+  };
+
+  handleSubmit = async (e) => {
+    e.preventDefault();
+    
+    const response = await api.post('/boxes', {
+      title: this.state.newBox,
+    });
+
+
+    console.log(response.data);
+  }
+
+  handleInputchange = (e) => {
+    this.setState({
+      newBox: e.target.value,
+    });
+  }
+
   render() {
     return (
       <div id="box-container">
-        <form action="">
+        <form onSubmit={this.handleSubmit} >
           <img src={logo} alt=""/>
-          <input placeholder="Criar box" />
+          <input 
+            placeholder="Criar box" 
+            value={this.state.newBox}
+            onChange={this.handleInputchange}
+          />
           <button type="submit">Criar</button>
         </form>
       </div>
